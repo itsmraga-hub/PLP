2b9f9efd9ee52d9a7c02eae46fc119fa9b2757fe
Raga <ragawilliam570@gmail.com>
2022-06-09 12:40:44 +0300

    First commit

---
+++ hospital.py
@@ -0,0 +1,8 @@
+name = "John Smith"
+age = 20
+isNew = True
+
+if isNew:
+    print(name + " is a new patient and is " + str(age) + " years old.")
+else:
+    print(name + " is a new patient and is " + str(age) + " years old.")
